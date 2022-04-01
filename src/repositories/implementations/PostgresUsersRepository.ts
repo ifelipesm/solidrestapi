c4655b2ff60bd15c449c3571c0670690e366b7c8
ifelipesm <ifelipesm@outlook.com>
2022-04-01 19:09:33 -0300

    Update app

--- src/repositories/implementations/PostgresUsersRepository.ts
+++ src/repositories/implementations/PostgresUsersRepository.ts
@@ -7,7 +7,7 @@ export  class PostgresUsersRepository implements IUsersRepository {
   async findByEmail(email: string): Promise<User>{
   const user = this.users.find(user => user.email === email);
 
-  return user;
+  return user; 
   }
 
   async save(user: User): Promise<void>{
